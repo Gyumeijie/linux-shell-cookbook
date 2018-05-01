51dce601ee53a5e96094515ef3d2b4fe53675a7c
Gyumeijie <18170879819@163.com>
2018-05-01 14:40:37 +0800

    feat: add zero-start range generating by `eval`

---
+++ basic/eval.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+start=0; end=10
+
+# only output {0..10}, not evaluate further
+# range={$start..$end}
+
+# use eval to make this work
+# output: 0 1 2 3 4 5 6 7 8 9 10
+range=`eval echo {$start..$end}`
+
+echo $range
